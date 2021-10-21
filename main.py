3d4ac97c19436cbd91a7e2eefe83a267323d1d3b
Александр Коновалов <rerik06@yandex.com>
2021-10-22 00:54:39 +0300

    Первый набросок нового лексера

--- main.py
+++ main.py
@@ -47,6 +47,57 @@ class Lexer:  # лексический анализ кода
         return result  # возвращает макет строки
 
 
+class NewLexer:
+
+    keywords: list
+    keywords_first_letters: str
+
+    def __init__(self, keywords: str = '', symbols: str = '') -> None:
+        self.keywords = keywords.split()
+        self.symbols  = symbols.split()
+        self.symbols.sort(key=lambda x: len(x), reverse=True)
+        print(self.symbols)
+        self.keywords_fl = "".join(set([keyword[0] for keyword in self.keywords])) # first letters
+        self.symbols_fl  = "".join(set([ symbol[0] for  symbol in self.symbols ])) # first letters
+
+    def parse(self, row: str) -> list:
+        row = row.split('//')[0] # чистка от строчных комментариев
+        row = re.sub(" +", " ", row.strip())
+        new_row = ''
+        row_len = len(row)
+        i = 0
+        while i <len(row):
+            if row[i] in self.symbols_fl:
+                found = False
+                for symbol in self.symbols:
+                    symbol_len = len(symbol)
+                    if i+symbol_len <= row_len:
+                        if row[i:i+symbol_len] == symbol:
+                            if new_row[-1] != ' ':
+                                new_row += ' '
+                            new_row += symbol + ' '
+                            i += symbol_len
+                            found = True
+                            break
+                if not found:
+                    i += 1
+            elif row[i] == ' ':
+                if row[i-1] != ' ':
+                    new_row += ' '
+                i += 1
+            else:
+                new_row += row[i]
+                i += 1
+
+        return new_row.strip().split()
+
+
+NL = NewLexer('if else end', '+= = -= <=')
+#print(NL.parse(' test    string  '))
+print(NL.parse(' test  <=string  '))
+#print(' test  string  ')
+
+
 class Node:  # управление узлами. Узлом является абстрактная модель,
     # представляющая исполняемую инструкцию и возможные переходы к другим инструкциям.
     # типы узлов
@@ -709,4 +760,4 @@ txt.pack(side=LEFT)
 scroll.pack(side=LEFT, fill=Y)
 # txt.config(yscrollcommand=scroll.set)
 
-root.mainloop()
+#root.mainloop()
